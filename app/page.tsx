d5fafd24e0e1d490c008631feba22e739eb7345d
jeonghun94 <jeongh1021@naver.com>
2024-07-05 17:26:56 +0900

    단축

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
     return (
-        <div className="flex flex-col   bg-red-100 text-purple-500 font-extrabold group">
-            <input type="text" className="border border-black" />
-            <span className="hidden group-focus-within:block">hidden</span>
-            <button>submit</button>
-
-            <div className="size-4 m-auto p-10 border border-blue-300">size</div>
+        <div className="flex flex-col w-full h-screen">
+            <div className="f rounded-full size-4 m-auto p-10 border border-blue-300">Test</div>
         </div>
     );
 }
